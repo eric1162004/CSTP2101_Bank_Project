6f66f0180219639b7226beaace8d87d70f8a0427
Eric Cheung <eric1162004@hotmail.com>
2021-07-24 10:49:10 -0700

    add db init database and tables

---
+++ data/init.sql
@@ -0,0 +1,63 @@
+DROP DATABASE IF EXISTS Bank;
+
+CREATE DATABASE Bank;
+
+USE Bank;
+
+CREATE TABLE Customer (
+    customerID INT NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(30) DEFAULT '',
+    lastName VARCHAR(30) DEFAULT '',
+    income DOUBLE DEFAULT 0.00,
+    birthDate DATE,
+    PRIMARY KEY (customerID)
+);
+
+CREATE TABLE Employee (
+    sin CHAR(9) NOT NULL, 
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
+    salary DOUBLE DEFAULT 0.00,
+    branchNumber INT, 
+    PRIMARY KEY (sin)
+);
+
+CREATE TABLE Branch (
+    branchNumber INT NOT NULL AUTO_INCREMENT,
+    branchName VARCHAR(30) NOT NULL,
+    managerSIN CHAR(9),
+    budget DOUBLE DEFAULT 0.00,
+    PRIMARY KEY (branchNumber),
+    FOREIGN KEY (managerSIN) REFERENCES Employee (sin)
+);
+
+ALTER TABLE Employee
+ADD FOREIGN KEY (branchNumber) REFERENCES Branch(branchNumber);
+
+CREATE TABLE Account (
+    accNumber INT NOT NULL AUTO_INCREMENT,
+    type ENUM('chequing','saving','business'), 
+    balance DOUBLE DEFAULT 0.00,
+    branchNumber INT,
+    PRIMARY KEY (accNumber),
+    FOREIGN KEY (branchNumber) REFERENCES Branch (branchNumber)
+);
+
+CREATE TABLE Owns (
+    customerID INT,
+    accNumber INT,
+    PRIMARY KEY (customerID, accNumber),
+    FOREIGN KEY (customerID) REFERENCES Customer (customerID),
+    FOREIGN KEY (accNumber) REFERENCES Account (accNumber)
+);
+
+CREATE TABLE Transactions (
+    transNumber INT,
+    accNumber INT,
+    amount DOUBLE DEFAULT 0.00,
+    PRIMARY KEY (transNumber, accNumber),
+    FOREIGN KEY (accNumber) REFERENCES Account (accNumber)
+);
+
+
+

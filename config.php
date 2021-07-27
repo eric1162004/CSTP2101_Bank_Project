38c4300991d0f84242d13a01fe7ec32a5c068d61
Eric Cheung <eric1162004@hotmail.com>
2021-07-27 10:29:36 -0700

    remove config.php from gitignore

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+//mysql://b8777d0ae7827a:be3a7d4b@us-cdbr-east-04.cleardb.com/heroku_f9e3d90d4a52b4f?reconnect=true
+ 
+$host       = "us-cdbr-east-04.cleardb.com";
+$username   = "b8777d0ae7827a";
+$password   = "be3a7d4b";
+$dbname     = "heroku_f9e3d90d4a52b4f";
+$dsn        = "mysql:host=$host;dbname=$dbname";
+
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );

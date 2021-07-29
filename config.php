dd3766d3c718b1c753540ddad313e66d197619ae
Eric Cheung <eric1162004@hotmail.com>
2021-07-28 19:10:42 -0700

    switch config

--- config.php
+++ config.php
@@ -2,15 +2,15 @@
 
 //mysql://b8777d0ae7827a:be3a7d4b@us-cdbr-east-04.cleardb.com/heroku_f9e3d90d4a52b4f?reconnect=true
  
-// $host       = "us-cdbr-east-04.cleardb.com";
-// $username   = "b8777d0ae7827a";
-// $password   = "be3a7d4b";
-// $dbname     = "heroku_f9e3d90d4a52b4f";
+$host       = "us-cdbr-east-04.cleardb.com";
+$username   = "b8777d0ae7827a";
+$password   = "be3a7d4b";
+$dbname     = "heroku_f9e3d90d4a52b4f";
 
-$host       = "localhost";
-$username   = "eric";
-$password   = "123456";
-$dbname     = "Bank";
+// $host       = "localhost";
+// $username   = "eric";
+// $password   = "123456";
+// $dbname     = "Bank";
 
 $dsn        = "mysql:host=$host;dbname=$dbname";
 
